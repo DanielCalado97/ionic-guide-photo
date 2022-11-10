f9573cc6c3e6bc2e8f53e67beaa2628623029f9c
DanielCalado97 <daniel.calado@outsystems.com>
2022-11-10 17:58:24 +0000

    Update usePhotoGallery.ts

--- photo-gallery/src/hooks/usePhotoGallery.ts
+++ photo-gallery/src/hooks/usePhotoGallery.ts
@@ -6,6 +6,7 @@ import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
 import { Capacitor } from '@capacitor/core';
 
+const PHOTO_STORAGE = 'photos';
 export function usePhotoGallery() {
   const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
     const base64Data = await base64FromPath(photo.webPath!);
@@ -23,7 +24,25 @@ export function usePhotoGallery() {
     };
   };
     const [photos, setPhotos] = useState<UserPhoto[]>([]);
+    useEffect(() => {
+      const loadSaved = async () => {
+        const { value } = await Preferences.get({ key: PHOTO_STORAGE });
+        const photosInPreferences = (value ? JSON.parse(value) : []) as UserPhoto[];
+    
+        for (let photo of photosInPreferences) {
+          const file = await Filesystem.readFile({
+            path: photo.filepath,
+            directory: Directory.Data,
+          });
+          // Web platform only: Load the photo as base64 data
+          photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+        }
+        setPhotos(photosInPreferences);
+      };
+      loadSaved();
+    }, []);
     const takePhoto = async () => {
+      
       const photo = await Camera.getPhoto({
         resultType: CameraResultType.Uri,
         source: CameraSource.Camera,
@@ -33,6 +52,7 @@ export function usePhotoGallery() {
       const savedFileImage = await savePicture(photo, fileName);
       const newPhotos = [savedFileImage, ...photos];
       setPhotos(newPhotos);
+      Preferences.set({ key: PHOTO_STORAGE, value: JSON.stringify(newPhotos) });
     };
     
   
@@ -62,6 +82,7 @@ export function usePhotoGallery() {
       reader.readAsDataURL(blob);
     });
   }
+  
   
 
   
