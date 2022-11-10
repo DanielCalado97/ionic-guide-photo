616cfe6da10d0a33e5d3a5d468f23b8c60fcad14
DanielCalado97 <daniel.calado@outsystems.com>
2022-11-10 17:07:43 +0000

    Update usePhotoGallery.ts

--- photo-gallery/src/hooks/usePhotoGallery.ts
+++ photo-gallery/src/hooks/usePhotoGallery.ts
@@ -7,6 +7,21 @@ import { Preferences } from '@capacitor/preferences';
 import { Capacitor } from '@capacitor/core';
 
 export function usePhotoGallery() {
+  const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
+    const base64Data = await base64FromPath(photo.webPath!);
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+
+    // Use webPath to display the new image instead of base64 since it's
+    // already loaded into memory
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath,
+    };
+  };
     const [photos, setPhotos] = useState<UserPhoto[]>([]);
     const takePhoto = async () => {
       const photo = await Camera.getPhoto({
@@ -15,13 +30,8 @@ export function usePhotoGallery() {
         quality: 100,
       });
       const fileName = new Date().getTime() + '.jpeg';
-      const newPhotos = [
-        {
-          filepath: fileName,
-          webviewPath: photo.webPath,
-        },
-        ...photos,
-      ];
+      const savedFileImage = await savePicture(photo, fileName);
+      const newPhotos = [savedFileImage, ...photos];
       setPhotos(newPhotos);
     };
     
@@ -36,4 +46,24 @@ export function usePhotoGallery() {
     filepath: string;
     webviewPath?: string;
   }
+  export async function base64FromPath(path: string): Promise<string> {
+    const response = await fetch(path);
+    const blob = await response.blob();
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        if (typeof reader.result === 'string') {
+          resolve(reader.result);
+        } else {
+          reject('method did not return a string');
+        }
+      };
+      reader.readAsDataURL(blob);
+    });
+  }
+  
+
+  
+  
 
