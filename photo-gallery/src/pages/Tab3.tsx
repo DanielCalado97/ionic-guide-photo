68aa69535ec492aecb8121134efaa3721fd8c2d3
DanielCalado97 <daniel.calado@outsystems.com>
2022-11-14 10:44:48 +0000

    Update Tab3.tsx

--- photo-gallery/src/pages/Tab3.tsx
+++ photo-gallery/src/pages/Tab3.tsx
@@ -1,7 +1,49 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
+import React from 'react';
+import {withIonLifeCycle } from '@ionic/react';
 
+class HomePage extends React.Component {
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter event fired');
+  }
+
+  ionViewWillLeave() {
+    console.log('ionViewWillLeave event fired');
+  }
+
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter event fired');
+  }
+
+  ionViewDidLeave() {
+    console.log('ionViewDidLeave event fired');
+  }
+
+  render() {
+    return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Tab 3</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Tab 3</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer name="Tab 3 page" />
+      </IonContent>
+    </IonPage>
+    );
+  }
+}
+
+//export withIonLifeCycle(HomePage);
+/*
 const Tab3: React.FC = () => {
   return (
     <IonPage>
@@ -21,5 +63,5 @@ const Tab3: React.FC = () => {
     </IonPage>
   );
 };
-
-export default Tab3;
+*/
+export default withIonLifeCycle(HomePage);
